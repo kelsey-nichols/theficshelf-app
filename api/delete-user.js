73c4eeacdd126959f026351174ffaaeeb08bcb0e
kdn0074 <kelseynichols2@my.unt.edu>
2025-06-08 02:07:06 -0500

    Minor UI.

--- api/delete-user.js
+++ api/delete-user.js
@@ -13,8 +13,8 @@ export default async function handler(req, res) {
 
   // Initialize an *admin* Supabase client
   const supabaseAdmin = createClient(
-    process.env.VITE_SUPABASE_URL,
-    process.env.VITE_SUPABASE_SERVICE_ROLE_KEY
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_SERVICE_ROLE_KEY
   );
 
   const { error } = await supabaseAdmin.auth.admin.deleteUser(userId);
